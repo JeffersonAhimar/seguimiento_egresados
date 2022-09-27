efe9a37c7f1b8a7f9034c6c003c18001c1cd9cc1
JeffersonAhimar <jeffersonahimar@outlook.com>
2022-09-27 17:32:26 -0500

    config example añadido

---
+++ config/config_example.php
@@ -0,0 +1,7 @@
+<?php
+// KEYS OF ACCESS
+define("MYSQL_HOST", 'localhost');
+define("MYSQL_PORT", '');
+define("MYSQL_USER", 'user');
+define("MYSQL_DATABASE", 'database');
+define("MYSQL_PASSWORD", 'pass');
